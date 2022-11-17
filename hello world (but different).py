9031e1d4b7c346eb25cb3a9c0f95e2997aed6a99
abrar018 <114000293+abrar018@users.noreply.github.com>
2022-11-16 18:24:08 -0700

    Add files via upload

---
+++ hello world (but different).py
@@ -0,0 +1,29 @@
+import time
+def c(string):
+    list1 = []
+    list1[:0] = string
+    return list1
+
+def f():
+    s='hello world'
+    s=c(s)
+    print(s)
+    i='abcdefghijklmnopqrstuvwxyz'
+    i=c(i)
+    n=['a','a','a','a','a',' ','a','a','a','a','a',]
+    g=''
+    
+    for k in range(len(s)):
+        if s[k]==' ':
+            continue
+        for j in i:
+            if n[k]!=s[k]:
+                n[k]=j
+                print(g.join(n[0:k+1]))
+                time.sleep(0.1)
+                
+f()
+
+
+            
+        
